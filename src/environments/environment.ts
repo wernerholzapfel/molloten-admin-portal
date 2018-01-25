3bf47284bcbab7ee68cf6a96714b76b43e40ef46
wholzapfel <werner.holzapfel@trivento.nl>
2018-01-25 22:47:42 +0100

    angular update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   api: 'http://molapi.herokuapp.com/api/v1'
 };
