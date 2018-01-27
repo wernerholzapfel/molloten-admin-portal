ebe1ca8f099afdbfbb24d675b371d39a105aaa29
wernerholzapfel <werner.holzapfel@gmail.com>
2018-01-27 23:01:46 +0100

    Redirect

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'WNjXlR4ChTqf2azaWhPk4MPzViNqoQft',
   domain: 'werner.eu.auth0.com',
-  callbackURL: 'http://localhost:4200/callback'
+  callbackURL: 'https://molloot-admin-portal.herokuapp.com'
 };
